ffcc44314814c6d37a4900d610232436b89216e4
egor <eg0r.n1k0l43v@gmail.com>
2017-05-13 00:02:33 +0700

    add enable concat config; make simple log

--- logs-spawner/spawner.js
+++ logs-spawner/spawner.js
@@ -3,11 +3,12 @@
  */
 
 const intervalId = setInterval(() => {
-  console.log('--\tOh, shit!\n\tI am multiline log.');
+  console.log('Simple log')
+  // console.log('--\tOh, shit!\n\tI am multiline log.');
 }, 500);
 
 
 process.on('SIGINT', function() {
   clearInterval(intervalId);
-  console.log('--\tBye!');
+  // console.log('--\tBye!');
 });
