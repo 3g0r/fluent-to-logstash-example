65f6edbdd287ae09ff3325a3348be5d68876371d
egor <eg0r.n1k0l43v@gmail.com>
2017-07-11 14:11:56 +0700

    correct works with flush_interval option

--- logs-spawner/spawner.js
+++ logs-spawner/spawner.js
@@ -2,12 +2,19 @@
  * Created by custo on 12.05.17.
  */
 
+const counters = {
+  intervalId: 0,
+};
+
 const intervalId = setInterval(() => {
   // console.log('Simple log')
   console.log('--\tOh, shit!');
-  console.log('\tI am multiline log.')
-}, 500);
+  console.log(counters.intervalId++);
+}, 6000);
 
+const interval2 = setInterval(() => {
+  console.log('--\tValidate' + counters.intervalId);
+}, 6001);
 
 process.on('SIGINT', function() {
   clearInterval(intervalId);
